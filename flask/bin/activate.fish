a2b499cd8d5233fc5e47effe8c0131945913dc2b
Vic <h82lin@uwaterloo.ca>
2017-08-25 18:13:24 -0400

    Change after setting up virtual environment

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/vic/jsr308/microblog/flask"
+set -gx VIRTUAL_ENV "/home/vic/jsr308/webproject/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
